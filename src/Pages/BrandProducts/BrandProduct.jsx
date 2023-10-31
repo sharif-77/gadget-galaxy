00525dc7292348c276725ef46398c71876394a2f
muhammad-sharif <muhammadsharif80400@gmail.com>
2023-10-31 16:00:48 +0600

    some feature is updated

--- src/Pages/BrandProducts/BrandProduct.jsx
+++ src/Pages/BrandProducts/BrandProduct.jsx
@@ -1,10 +1,8 @@
-import { useContext } from "react";
-import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 import { Link } from "react-router-dom";
 
 const BrandProduct = ({product}) => {
     const {_id,image,name,description}=product
-    const {mode}=useContext(AuthContext)
   return (
 
     <div>
@@ -17,8 +15,8 @@ const BrandProduct = ({product}) => {
           />
         </figure>
         <div className="card-body">
-          <h2 className={`${mode?`dark:text-black`:`text-black`} card-title`}>{name}</h2>
-          <p className={`${mode&&`text-black`} min-h-[100px]`}>{description}</p>
+          <h2 className= "card-title">{name}</h2>
+          <p className= "min-h-[100px]">{description}</p>
           <div className="card-actions justify-end">
             <Link to={`/details/${_id}`}  className="btn btn-primary">See Details</Link>
           </div>
